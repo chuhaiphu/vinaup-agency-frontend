2517b6110b210ec7ce386e0b53b0df66bbe05c82
PTT154 <nguyenminhphuoc154@gmail.com>
2026-05-12 08:06:00 +0700

    feat: initialize vitinhnet app with configuration files and dependencies

---
+++ apps/vitinhnet/next.config.ts
@@ -0,0 +1,73 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ['10.149.35.43', 'localhost:3000', '192.168.1.152'],
+  transpilePackages: ['@vinaup/ui', '@vinaup/utils'],
+  cacheComponents: true,
+  output: 'standalone',
+  reactCompiler: true,
+  devIndicators: {
+    position: 'bottom-right',
+  },
+  typedRoutes: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '6mb',
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.vinaup.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.jenahair.com',
+        pathname: '/**',
+      },
+    ],
+    localPatterns: [
+      {
+        pathname: '/**',
+      },
+    ],
+    unoptimized: process.env.NODE_ENV === 'development',
+  },
+
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
+};
+
+export default nextConfig;
