f6ac0256d9deccbc01c50b0e5d1068842850434b
PTT154 <nguyenminhphuoc154@gmail.com>
2026-06-11 22:21:16 +0700

    feat(vitinhnet): implement product comparison section

---
+++ apps/vitinhnet/src/app/(landing)/trang-so-sanh/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ProductCompare } from './product-compare'; // Sửa lại đường dẫn import nếu bạn để component ở thư mục khác
+
+export default function ComparePage() {
+    return (
+        <main>
+            <ProductCompare />
+        </main>
+    );
+}
